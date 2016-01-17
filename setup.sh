9e4499c16c8961c47d15e629a20368c4c0a36405
Marcus Hansson <marcus.hansson@email.com>
2016-01-17 16:57:27 +0000

    Fix plugin installation killed before finished

--- setup.sh
+++ setup.sh
@@ -5,10 +5,7 @@ function setup {
     link=~/.vimrc
 
     ln -s $target $link 1>&2 2>/dev/null
-    vim "+source $link" "+PluginInstall" "+qall" &
-
-    pid=$!
-    kill -9 $pid 1>&2 2>/dev/null
+    vim "+source $link" "+PluginInstall" "+qall" 1>&2 2>/dev/null &
 
     echo "Done"
 }
